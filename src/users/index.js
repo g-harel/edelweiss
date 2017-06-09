90d9ca7c7bad963e6ced7e4972878d1a2abbb7bc
Gabriel <pabombs@gmail.com>
2017-06-08 22:48:58 -0400

    started usre database api

---
+++ src/users/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const dbcon = () => {
+    const add = () => {
+
+    };
+
+    const edit = () => {
+
+    };
+
+    const remove = () => {
+
+    };
+
+    const auth = () => {
+
+    };
+
+    return {add, edit, remove, auth};
+};
+
+module.exports = dbcon;
