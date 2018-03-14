0b2000fc358e66a67bf01e8a5524b752d5b91143
Gabriel <gabrielj.harel@gmail.com>
2018-03-14 00:45:58 -0400

    added better check for executables in path

--- cmd/check.go
+++ cmd/check.go
@@ -1,17 +1,41 @@
 package cmd
 
 import (
+	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
 func checkFatal(commands ...string) {
-	for _, c := range commands {
-		_, err := run(c)
+	path, ok := os.LookupEnv("PATH")
+	if !ok {
+		color.Red("\ncould not read PATH\n\n")
+		os.Exit(1)
+	}
+
+	dirs := strings.Split(path, string(os.PathListSeparator))
+
+	exec := make(map[string]bool)
+	for _, d := range dirs {
+		files, err := ioutil.ReadDir(d)
 		if err != nil {
-			color.Red("\ndependency missing: %v: %v\n\n", c, err)
+			color.Red("\ncould not read directory (%v): %v\n\n", d, err)
+			os.Exit(1)
+		}
+
+		for _, f := range files {
+			if f.Mode()&0111 != 0 {
+				exec[f.Name()] = true
+			}
+		}
+	}
+
+	for _, c := range commands {
+		if !exec[c] {
+			color.Red("\ndependency missing: %v\n\n", c)
 			os.Exit(1)
 		}
 	}
