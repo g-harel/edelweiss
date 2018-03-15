81686f9c9f1d68b94752ac91c3facd4f269e8e50
Gabriel Harel <gabrielj.harel@gmail.com>
2018-03-15 03:07:42 -0400

    made checkFatal compatible with windows

--- cmd/check.go
+++ cmd/check.go
@@ -1,41 +1,18 @@
 package cmd
 
 import (
-	"io/ioutil"
 	"os"
-	"strings"
+	"os/exec"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
 func checkFatal(commands ...string) {
-	path, ok := os.LookupEnv("PATH")
-	if !ok {
-		color.Red("\ncould not read PATH\n\n")
-		os.Exit(1)
-	}
-
-	dirs := strings.Split(path, string(os.PathListSeparator))
-
-	exec := make(map[string]bool)
-	for _, d := range dirs {
-		files, err := ioutil.ReadDir(d)
-		if err != nil {
-			color.Red("\ncould not read directory (%v): %v\n\n", d, err)
-			os.Exit(1)
-		}
-
-		for _, f := range files {
-			if f.Mode()&0111 != 0 {
-				exec[f.Name()] = true
-			}
-		}
-	}
-
 	for _, c := range commands {
-		if !exec[c] {
-			color.Red("\ndependency missing: %v\n\n", c)
+		_, err := exec.LookPath(c)
+		if err != nil {
+			color.Red("\ncheckFatal: %s\n(If you are on windows, executables must have a file extension to be found)\n\n", err)
 			os.Exit(1)
 		}
 	}
